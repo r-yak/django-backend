11d044027374e010f3e5fae8be7e351f1eca8e0b
Hepheir <hepheir@gmail.com>
2023-08-31 03:48:32 +0900

    feat(config): add '*' to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=0i*%mn)2(ivzjgm252_amt-u8qcz@)1dd6!$xyb$*@#poxz1%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
