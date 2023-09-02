c97ecb6c2dd8d1354afc69c2362d77ac7231612b
Hepheir <hepheir@gmail.com>
2023-09-03 01:16:05 +0900

    refactor(api): change DrugPredictionSerializer -> PredictionSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from api.models import Drug
+from api.models import Drug, Prediction
 
 
 class DrugSerializer(serializers.ModelSerializer):
@@ -9,36 +9,7 @@ class DrugSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class DrugPredictionSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(source='ITEM_NAME')
-    dosage_form = serializers.SerializerMethodField()
-    dosage = serializers.SerializerMethodField()
-    dosage_unit = serializers.SerializerMethodField()
-    shape = serializers.SerializerMethodField()
-    color = serializers.SerializerMethodField()
-
+class PredictionSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Drug
-        fields = [
-            'name',
-            'dosage_form',
-            'dosage',
-            'dosage_unit',
-            'shape',
-            'color',
-        ]
-
-    def get_dosage_form(self, obj: Drug):
-        return ''
-
-    def get_dosage(self, obj: Drug):
-        return ''
-
-    def get_dosage_unit(self, obj: Drug):
-        return ''
-
-    def get_shape(self, obj: Drug):
-        return ''
-
-    def get_color(self, obj: Drug):
-        return ''
+        model = Prediction
+        fields = '__all__'
