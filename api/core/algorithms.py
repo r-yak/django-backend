05cb0f518f677992ac8538345ccbf24ecd505e0e
Hepheir <hepheir@gmail.com>
2023-09-03 08:32:28 +0900

    refactor(api/core): rather use "munsell_specification" than "munsell_color" (string) to avoid colorless detection failing

--- api/core/algorithms.py
+++ api/core/algorithms.py
@@ -1,9 +1,14 @@
+import dataclasses
 import functools
+import re
 import typing
 
+import colorgram
+import colour
 import cv2
 import cv2.typing
 import numpy
+import PIL.Image
 
 from api.core import exceptions
 from api.models import ColorChoices, Drug, ShapeChoices
@@ -11,45 +16,39 @@ from api.models import ColorChoices, Drug, ShapeChoices
 
 @dataclasses.dataclass
 class _MunsellColor:
-    step: float
-    hue: str
+    hue: float
     value: float
     chroma: float
+    code: float
 
-    def is_colorless(self) -> bool:
-        return self.chroma < 4.0 or self.value < 2.0 or self.value > 9.0
+    COLOR_CHOICES_MUNSELL_HUE_CODES = {
+        2: ColorChoices.BLUE_GREEN, # "BG"
+        4: ColorChoices.YELLOW_GREEN, # "GY"
+        6: ColorChoices.ORANGE, # "YR"
+        8: ColorChoices.REDDISH_PURPLE, # "RP"
+        10: ColorChoices.BLUISH_VIOLET, # "PB"
+        1: ColorChoices.BLUE, # "B"
+        3: ColorChoices.GREEN, # "G"
+        5: ColorChoices.YELLOW, # "Y"
+        7: ColorChoices.RED, # "R"
+        9: ColorChoices.BLUISH_PURPLE, # "P"
+    }
 
     def to_color_choice(self) -> ColorChoices:
-        if self.is_colorless():
-            if self.value < 2.5:
-                return ColorChoices.BLACK
-            elif self.value < 7.0:
-                return ColorChoices.GRAY
-            else:
-                return ColorChoices.WHITE
-        match self.hue.lower():
-            case 'r':
-                return ColorChoices.RED
-            case 'yr':
-                return ColorChoices.ORANGE
-            case 'y':
-                return ColorChoices.YELLOW
-            case 'gy':
-                return ColorChoices.YELLOW_GREEN
-            case 'g':
-                return ColorChoices.GREEN
-            case 'bg':
-                return ColorChoices.BLUE_GREEN
-            case 'b':
-                return ColorChoices.BLUE
-            case 'pb':
-                return ColorChoices.BLUISH_VIOLET
-            case 'p':
-                return ColorChoices.BLUISH_PURPLE
-            case 'rp':
-                return ColorChoices.REDDISH_PURPLE
-            case _:
-                return ColorChoices.UNKNOWN
+        if self._is_colorless():
+            return self._to_color_choices_colorless()
+        return __class__.COLOR_CHOICES_MUNSELL_HUE_CODES[round(self.code, 1)]
+
+    def _is_colorless(self) -> bool:
+        return self.chroma < 4.0 or self.value < 2.0 or self.value > 9.0 or round(self.code, 1) == 0
+
+    def _to_color_choices_colorless(self) -> ColorChoices:
+        if self.value < 2.5:
+            return ColorChoices.BLACK
+        elif self.value < 7.0:
+            return ColorChoices.GRAY
+        else:
+            return ColorChoices.WHITE
 
 
 class PredictionModel:
@@ -124,18 +123,13 @@ class PredictionModel:
         color_rgb = list(map(lambda x: x / 255.0, color.rgb))
         color_XYZ = colour.RGB_to_XYZ(color_rgb, colour.models.RGB_COLOURSPACE_sRGB)
         color_xyY = colour.XYZ_to_xyY(color_XYZ)
-        color_munsell = colour.xyY_to_munsell_colour(color_xyY)
-        m = self._get_munsell_pattern().match(color_munsell)
+        color_munsell = colour.notation.munsell.xyY_to_munsell_specification(color_xyY)
         return _MunsellColor(
-            step=float(m.group('step')),
-            hue=m.group('hue'),
-            value=float(m.group('value')),
-            chroma=float(m.group('chroma')),
+            hue=color_munsell[0],
+            value=color_munsell[1],
+            chroma=color_munsell[2],
+            code=color_munsell[3],
         )
-
-    @functools.cache
-    def _get_munsell_pattern(self) -> re.Pattern:
-        return re.compile(r"(?P<step>(\d*\.)?\d+)(?P<hue>\w+) (?P<value>(\d*\.)?\d+)/(?P<chroma>(\d*\.)?\d+)")
 
     def predict_drug(self) -> typing.Optional[Drug]:
         return None
