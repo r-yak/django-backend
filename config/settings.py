3d4ef7ea595ecf643d09c95f83b9ecc5f0840615
Hepheir <hepheir@gmail.com>
2023-09-03 17:17:36 +0900

    chore(config): add CORS_ALLOW_CREDENTIALS, CORS_ORIGIN_ALLOW_ALL, CORS_ALLOW_METHODS

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS'
+]
+
 
 # Application definition
 
